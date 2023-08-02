6d186547a5f442f0f9ca7603570d3cafc12f0151
ergophile-geek <hawanatamang@gmail.com>
2023-08-03 00:08:52 +0545

    code: Add image code

--- Music-Verse/src/dev/musicVerse/Design.java
+++ Music-Verse/src/dev/musicVerse/Design.java
@@ -2,6 +2,7 @@ package dev.musicVerse;
 
 import javax.swing.*;
 import javax.swing.border.AbstractBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.RoundRectangle2D;
@@ -10,6 +11,13 @@ public class Design extends JFrame{
     private static  final Container container = new Container();
     private static final JPanel panel = new JPanel();
     private Point mousePressLocation;
+    JPanel imgpnl = new JPanel(){
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            Image image = new ImageIcon(this.getClass().getResource("Images/edge.jpg")).getImage();
+            g.drawImage(image,0,0,this.getWidth(),this.getHeight(),this);
+        }
+    };
     public Design(){
         //Colors properties
         Color backgroundColor = Color.decode("#00000");
@@ -20,13 +28,19 @@ public class Design extends JFrame{
         addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
-                setShape(new RoundRectangle2D.Double(0,0,getWidth(),getHeight(),50,50));
+                setShape(new RoundRectangle2D.Double(0,0,getWidth(),getHeight(),20,20));
             }
         });
 
         setSize(1280,780);
         setBackground(backgroundColor);
         setLocation(120,20);
+
+        imgpnl.setBackground(Color.WHITE);
+        imgpnl.setBorder(new EmptyBorder(5,5,5,5));
+        imgpnl.setLayout(null);
+        container.add(imgpnl);
+
 
         //Add mouse listener to handle dragging (moving) the frame
         addMouseListener(new MouseAdapter() {
@@ -48,11 +62,16 @@ public class Design extends JFrame{
             }
         });
 
-
-
+        //left panel
         panel.setBounds(0,0,280,800);
         panel.setBackground(panelColor);
         container.add(panel);
+
+        //Image
+//        imgpnl.setBackground(Color.WHITE);
+//        imgpnl.setBorder(new EmptyBorder(5,5,5,5));
+//        imgpnl.setLayout(null);
+//        container.add(imgpnl);
 
         setLayout(null);
         setContentPane(container);
