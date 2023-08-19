128d21ccfb8def5228cd48041b5c51e91d11ce52
ergophile-geek <hawanatamang@gmail.com>
2023-08-20 00:47:24 +0545

    feat: Add RoundedButton code

--- Music-Verse/src/dev/musicVerse/RoundedButton.java
+++ Music-Verse/src/dev/musicVerse/RoundedButton.java
@@ -1,2 +1,36 @@
-package dev.musicVerse;public class RoundedButton {
+package dev.musicVerse;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedButton extends JButton {
+
+    public RoundedButton(String text) {
+        super(text);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setOpaque(false);
+
+        setPreferredSize(new Dimension(120, 40));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 20, 20));
+
+        g2.setColor(getForeground());
+        FontMetrics metrics = g2.getFontMetrics(getFont());
+        int x = (getWidth() - metrics.stringWidth(getText())) / 2;
+        int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
+        g2.drawString(getText(), x, y);
+
+        g2.dispose();
+    }
 }
+
+
